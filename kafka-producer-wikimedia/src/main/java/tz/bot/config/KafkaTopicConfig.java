42a1e22b12f7e129c0dca4dff43594f74e2cc30c
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-30 18:06:49 +0530

    Created Wikimedia Topic

---
+++ kafka-producer-wikimedia/src/main/java/tz/bot/config/KafkaTopicConfig.java
@@ -0,0 +1,19 @@
+package tz.bot.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    private String topicName="wikimedia_recentchanges";
+
+    public NewTopic topic(){
+        return TopicBuilder
+                .name(topicName)
+                .replicas(1)
+                .partitions(2)
+                .build();
+    }
+}
