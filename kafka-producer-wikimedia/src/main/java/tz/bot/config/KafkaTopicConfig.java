905c261ac66d235f55a799390e14b4e00a7d60d2
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-30 18:13:11 +0530

    Added Dependencies in Producer Module for OkHttp and Jackson

--- kafka-producer-wikimedia/src/main/java/tz/bot/config/KafkaTopicConfig.java
+++ kafka-producer-wikimedia/src/main/java/tz/bot/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package tz.bot.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
@@ -8,7 +9,7 @@ import org.springframework.kafka.config.TopicBuilder;
 public class KafkaTopicConfig {
 
     private String topicName="wikimedia_recentchanges";
-
+    @Bean
     public NewTopic topic(){
         return TopicBuilder
                 .name(topicName)
