f089a86ef4510573d739b3f78f7e41b0b893c087
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-30 18:55:05 +0530

    Wikimedia Main File Fix

--- kafka-producer-wikimedia/src/main/java/tz/bot/SpringBootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/tz/bot/SpringBootProducerApplication.java
@@ -1,13 +1,25 @@
 package tz.bot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tz.bot.kafka.WikimediaChangesProducer;
 
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class);
         System.out.println("Test");
+    }
+
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
