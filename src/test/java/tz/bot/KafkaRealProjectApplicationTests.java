5310c1db607cfd8e7c34b996d91d45198d3729ba
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-30 18:04:27 +0530

    Initial commit

---
+++ src/test/java/tz/bot/KafkaRealProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tz.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRealProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
